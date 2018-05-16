23816c496bf9862335caa82131bb96414008c757
Zig Tan <zig@moodle.com>
2018-05-16 13:19:22 +0800

    MDL-49593 mod_assign: Fix grading using unlimited grades
    Many thanks to Matt McDermott for investigating and providing the initial solution.

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -2440,7 +2440,7 @@ class assign {
      * @return bool true for success
      */
     public function update_grade($grade, $reopenattempt = false) {
-        global $DB;
+        global $CFG, $DB;
 
         $grade->timemodified = time();
 
@@ -2455,9 +2455,11 @@ class assign {
             if ($this->get_instance()->grade > 0) {
                 if (!is_numeric($grade->grade)) {
                     return false;
-                } else if ($grade->grade > $this->get_instance()->grade) {
-                    return false;
                 } else if ($grade->grade < 0) {
+                    return false;
+                } else if ($CFG->unlimitedgrades && ($grade->grade > $CFG->gradepointmax)) {
+                    return false;
+                } else if (!$CFG->unlimitedgrades && ($grade->grade > $this->get_instance()->grade)) {
                     return false;
                 }
             } else {
