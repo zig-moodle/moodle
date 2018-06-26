1ccb75079c7179c8da98d2e036b3ec7af235ee81
Zig Tan <zig@moodle.com>
2018-06-26 10:42:11 +0800

    MDL-61592 files: Improve regex for filenames in zip download
    - Improved mangle_pathname() regex to:
      - Replace './../..././' with '/' for localname (and combinations thereof)
      - Remove '...' without mangling file extension
    - Updated zip packer unit tests

--- lib/filestorage/tests/zip_packer_test.php
+++ lib/filestorage/tests/zip_packer_test.php
@@ -52,6 +52,8 @@ class core_files_zip_packer_testcase extends advanced_testcase implements file_p
 
         $this->files = array(
             'test.test' => $this->testfile,
+            'test..file.txt' => $this->testfile,
+            'testfile..txt' => $this->testfile,
             'testíček.txt' => $this->testfile,
             'Prüfung.txt' => $this->testfile,
             '测试.txt' => $this->testfile,
@@ -113,7 +115,7 @@ class core_files_zip_packer_testcase extends advanced_testcase implements file_p
         $this->assertTrue(is_array($archivefiles), "Archive not extracted properly: ".basename($archive).' ');
         $this->assertEquals(2, count($archivefiles));
         foreach ($archivefiles as $file) {
-            $this->assertTrue($file->pathname === 'Prüfung.txt' or $file->pathname === 'test.test');
+            $this->assertArrayHasKey($file->pathname, $this->files);
         }
 
         $zip_archive = new zip_archive();
@@ -122,7 +124,7 @@ class core_files_zip_packer_testcase extends advanced_testcase implements file_p
         $this->assertTrue(is_array($archivefiles), "Archive not extracted properly: ".basename($archive).' ');
         $this->assertEquals(3, count($archivefiles));
         foreach ($archivefiles as $file) {
-            $this->assertTrue($file->pathname === 'Žluťoučký/Koníček.txt' or $file->pathname === 'testíček.txt' or $file->pathname === 'test.test');
+            $this->assertArrayHasKey($file->pathname, $this->files);
         }
         $zip_archive->close();
 
@@ -558,6 +560,8 @@ class core_files_zip_packer_testcase extends advanced_testcase implements file_p
         file_put_contents($subfolder . '/test3.txt', 'and');
         file_put_contents($subfolder . '/test4.txt', 'other');
         file_put_contents($subfolder . '/test5.txt', 'worlds');
+        file_put_contents($subfolder . '/test6.txt', 'and');
+        file_put_contents($subfolder . '/test7.txt', 'dimensions');
         $this->progress = array();
         $result = $packer->archive_to_pathname(array('' => $folder), $archive, true, $this);
         $this->assertTrue($result);
